3a8b8c14dfa440dcdb5df0982473de1f5450c0cf
s17571 <s17571@pjwstk.edu.pl>
2020-03-22 20:07:53 +0100

    init

---
+++ WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        /*
+
+        //zadanie 3
+
+        [HttpGet]
+        public String GetStudents()
+        {
+            return "Kowalski, Nowak, Lewandowski";
+        }
+
+        */
+
+        /*
+
+        //zadanie 4
+
+        [HttpGet]
+        public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        */
+
+        /*
+
+        // zadanie 5
+
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        }
+
+        */
+
+        // zadanie 6
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            // add to database
+            // generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+    }
+}
